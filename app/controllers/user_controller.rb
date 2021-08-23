15f80931bd1defa7de6c829f61edc11bf7cde3ff
Bilanuk <bilanuk.bill@gmail.com>
2021-08-23 21:55:28 +0300

    added_user_session_validation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,14 @@
 class UserController < ApplicationController
   def index
-    @users = User.all
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 
   def show
-    #to do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 
   def new
@@ -14,6 +18,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
@@ -23,6 +28,6 @@ class UserController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :userName)
   end
 end
