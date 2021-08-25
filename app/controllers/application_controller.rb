737e9048028a0832a7559b0f6cfb42d30fa45b76
Bilanuk <bilanuk.bill@gmail.com>
2021-08-25 16:56:54 +0300

    added_password_update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
       Current.user = User.find_by(id: session[:user_id])
     end
   end
+
+  def require_user_logged_in!
+    if Current.user.nil?
+      redirect_to new_session_path, alert: "You have to log in firstly"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "user#index"
   delete "logout", to: "sessions#destroy"
+
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update"
+
   resources :articles, :user, :sessions
 end
